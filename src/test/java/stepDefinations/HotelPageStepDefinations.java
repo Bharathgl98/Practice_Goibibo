c13abe586b569e96e38ef19f325c1ba1d27d71ef
bharath <bharath131926@gmail.com>
2024-10-14 11:31:09 +0530

    goibibo first commit

---
+++ src/test/java/stepDefinations/HotelPageStepDefinations.java
@@ -0,0 +1,52 @@
+package stepDefinations;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HotelPageStepDefinations {
+
+	@Given("user is on the Goibibo hotel search page")
+	public void user_is_on_the_goibibo_hotel_search_page() {
+
+	}
+
+	@When("user click on India")
+	public void user_click_on_india() {
+
+	}
+
+	@When("user enter the location {string}")
+	public void user_enter_the_location(String string) {
+
+	}
+
+	@When("user select the check-in date {string}")
+	public void user_select_the_check_in_date(String string) {
+
+	}
+
+	@When("user select the check-out date {string}")
+	public void user_select_the_check_out_date(String string) {
+
+	}
+
+	@When("user select the number of Guests and Rooms")
+	public void user_select_the_number_of_guests_and_rooms() {
+
+	}
+
+	@When("user click on the search button")
+	public void user_click_on_the_search_button() {
+
+	}
+
+	@Then("user should see a list of hotels in {string}")
+	public void user_should_see_a_list_of_hotels_in(String string) {
+
+	}
+
+	
+
+
+}
